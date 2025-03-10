41d0741152ca2a75958142623bc3cf3afed00919
SageHourihan <samiho97@gmail.com>
2025-03-10 18:52:22 +0000

    getting funds from front-end and setting up getFunds class

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php include_once 'views/header.html' ?>
  <body>
     <script src="index.js"></script>
-    <input type='text'></input>
+    <input id='funds' type='text'></input>
     <button id='submit'>Submit</button>
   </body>
 </html>
--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
     $('#submit').on('click', function(){
-        alert('hello');
+        let funds = $('#funds').val();
+        $.ajax ({
+            url: '../../src/Services/getFunds.php',
+            type: 'GET',
+            data: {funds:funds},
+            success: function(response){
+                console.log(response);
+            },
+            error: function(xhr, status, error){
+                console.log("AJAX Error: ", xhr,  status, error);
+            }
+        });
     })
 });
